144dd5e17fa2e056dc1a4164fd0e73e579802d9b
NBM69 <71622790+NBM69@users.noreply.github.com>
2021-02-20 14:47:04 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,14 +2,17 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1439732384:AAE1l81bvkSXxolwaH7T6Yt2gBcjNVdgbXI"
-	APP_ID = 2290897
-	API_HASH = "edac79cb30e9d4c106f19b7fc56c76d7"
-	OWNER_ID = "1494222126"
-	AUTH_CHANNEL = [-1001343804070]
-	DESTINATION_FOLDER = "!Bot Upload"
-	RCLONE_CONFIG = """type = drive
-client_id = 382912415643-6fshe2g7fv96rds3op7kpuemlftjhogv.apps.googleusercontent.com
-client_secret = PwlxTgScJ4umzYSKC-0qyuGD
+	TG_BOT_TOKEN= "1590787120:AAHJzv94-fAvcf9yHREf-XUtXoM0R6g-OaA"
+    APP_ID = 1587678
+    API_HASH = "67d9dac056afc4666ffd6b3474779c9d"
+    OWNER_ID = 1322198781
+    AUTH_CHANNEL = [-1001266692988]
+    DESTINATION_FOLDER = "!Bot Upload" 
+    RCLONE_CONFIG = "type = drive
+client_id = 33215714649-b4h7p2vv9f7a8abd0lm00jan63c4ik37.apps.googleusercontent.com
+client_secret = pswfZ5p3_5xWdysO8vW4LxGX
 scope = drive
-token = {"access_token":"ya29.a0AfH6SMC9FUyTlU2ArLNBBTOQkbnGmHHIYy-6PGvrh_5qacIHGUy9RBT-iQotPt8cjyvx-3uqf_JjHZBecoNwV1tU40LDZzX1XhtGtAxmrrHfX6vLwRIZqQ14vQQn34xKUyTilMkBIOHGhYu0HIDVunZOElF6Fx1vo6BS_D6JGZQ","token_type":"Bearer","refresh_token":"1//0diT6l-UIlGSGCgYIARAAGA0SNwF-L9IraFBm-Zbk7GUXU--CkJj649SGz_A79na14kTysI9uT1B_ZCJVqI9V9-JPj3rfZ4cJhBg","expiry":"2020-12-12T05:35:21.055194388Z"} """
+root_folder_id = root
+token = {"access_token":"ya29.A0AfH6SMDYiRRqk8tzLCnhuGcsrPsKcyBAILs1_xY--M0YyNt9T273DJvbR_bJkMqJ5SWJDCbroyP3Ztzsvf4tTUE3ZUakUlQthAdZXp9C8Otmt6WCnyD2yJxTMJHtt5Fihr_rQe835SMSjoXx6q87duo-2Ors","token_type":"Bearer","refresh_token":"1//0dTX1xqO7-qITCgYIARAAGA0SNwF-L9IryqViHjJPlg2LmLsDcBU6U8CriY1aMmbDdCOl4I_PyKxJjDhwr99nIbezSHtsyFBd2h4","expiry":"2021-02-20T06:04:03.52284836Z"}
+team_drive = 0ACcSs3TunG8tUk9PVA"
+© 2021 GitHub, Inc.
