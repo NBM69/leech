523d13534bd9f18156c12e2afc96b9373108112a
NBM69 <71622790+NBM69@users.noreply.github.com>
2021-02-20 14:43:18 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,11 +2,17 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1268fill---your----data"
-	APP_ID = 1373938
-	API_HASH = "fill--your--data"
-	OWNER_ID = "" #ID of bot owner
-	AUTH_CHANNEL = [-10082786282972]
-	DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
-	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
-	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
+	TG_BOT_TOKEN= "1590787120:AAHJzv94-fAvcf9yHREf-XUtXoM0R6g-OaA"
+    APP_ID = 1587678
+    API_HASH = "67d9dac056afc4666ffd6b3474779c9d"
+    OWNER_ID = 1322198781
+    AUTH_CHANNEL = [-1001266692988]
+    DESTINATION_FOLDER = "!Bot Upload" 
+    RCLONE_CONFIG = "type = drive
+client_id = 33215714649-b4h7p2vv9f7a8abd0lm00jan63c4ik37.apps.googleusercontent.com
+client_secret = pswfZ5p3_5xWdysO8vW4LxGX
+scope = drive
+root_folder_id = root
+token = {"access_token":"ya29.A0AfH6SMDYiRRqk8tzLCnhuGcsrPsKcyBAILs1_xY--M0YyNt9T273DJvbR_bJkMqJ5SWJDCbroyP3Ztzsvf4tTUE3ZUakUlQthAdZXp9C8Otmt6WCnyD2yJxTMJHtt5Fihr_rQe835SMSjoXx6q87duo-2Ors","token_type":"Bearer","refresh_token":"1//0dTX1xqO7-qITCgYIARAAGA0SNwF-L9IryqViHjJPlg2LmLsDcBU6U8CriY1aMmbDdCOl4I_PyKxJjDhwr99nIbezSHtsyFBd2h4","expiry":"2021-02-20T06:04:03.52284836Z"}
+team_drive = 0ACcSs3TunG8tUk9PVA"
+© 2021 GitHub, Inc.
